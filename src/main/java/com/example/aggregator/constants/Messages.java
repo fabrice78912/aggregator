75d22c5db0795752c71a3949c493f5bee2b0db8a
fabrice78912 <fabricenokam@gmail.com>
2025-09-07 13:31:45 -0400

    first commit

---
+++ src/main/java/com/example/aggregator/constants/Messages.java
@@ -0,0 +1,10 @@
+package com.example.aggregator.constants;
+
+
+public final class Messages {
+    private Messages() {}
+
+    public static final String SERVICE_UNAVAILABLE_MESSAGE = "%s indisponible pour client %s";
+    public static final String SERVICE_ERROR_MESSAGE = "%s error: %s";
+}
+
